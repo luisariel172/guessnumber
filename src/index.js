83be8f30f5c5dfad854e1a7d4174fccba55c9fc1
Ariel <sitioslaa@gmail.com>
2022-11-21 02:44:46 -0300

    hasta creacion header

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import { StyleSheet, View } from 'react-native';
+import { Header } from './components';
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Header title='Bienvenidos' />
+    </View>
+  );
+} 
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+  },
+});
