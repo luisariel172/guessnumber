3316361dda5a329c78dc378da800447ee8162d16
Ariel <sitioslaa@gmail.com>
2022-12-14 12:15:39 -0300

    GuessNumbers finished

--- src/screens/game-over/index.js
+++ src/screens/game-over/index.js
@@ -1,11 +1,24 @@
 import React from "react";
-import { View, Text, Image, Card, Button } from "react-native";
+import { View, Text, Image, Button } from "react-native";
 import { styles } from "./styles";
+import { Card } from "../../components";
+import colors from "../../utils/colors";
 
-const GameOver = ({round, seleectedNumber, onRestart}) => {
+const GameOver = ({round, selectedNumber, onRestart}) => {
     return(
         <View style={styles.container}>
-            <Text>Fin del Juego</Text>
+            <Card style={styles.content}>
+                <Image 
+                    source={{ uri: 'https://play-lh.googleusercontent.com/shAAX3m_CJQyahe1eIochbdEqH7FDiLEQ9i5WdI8DwNj2N9auke35k8CRmyL0mh7ULs' }}
+                    style={styles.image} />
+                <Text style={styles.textContent}> Rounds: {round} </Text>
+                <Text style={styles.textContent}> Numero Seleccionado: {selectedNumber}</Text>
+                <Button 
+                    title="Jugar de Nuevo"
+                    onPress={onRestart}
+                    color= {colors.primary}
+                />
+            </Card>
         </View>
     )
 }
